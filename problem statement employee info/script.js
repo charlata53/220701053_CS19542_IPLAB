8bfdcf87bf0a18456b86a0fb9ad418dbb767a367
charlata53 <220701053@rajalakshmi.edu.in>
2024-08-07 09:17:15 +0530

    Add files via upload

---
+++ problem statement employee info/script.js
@@ -0,0 +1,43 @@
+
+// Function to capitalize the first letter of a string
+function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+// Function to generate email, username, and password
+function generateInfo(firstName, lastName) {
+    const email = `${firstName.toLowerCase()}.${lastName.toLowerCase()}@abc.com`;
+    const username = `${firstName.toLowerCase()}${capitalizeFirstLetter(lastName)}`;
+    const password = `${lastName.toLowerCase()}#${capitalizeFirstLetter(firstName)}`;
+
+    return { email, username, password };
+}
+
+// Function to display the formatted information
+function displayInfo(names) {
+    const outputElement = document.getElementById('solution');
+    let outputHTML = '';
+
+    names.forEach(name => {
+        const [firstName, lastName] = name.split('_');
+        const { email, username, password } = generateInfo(firstName, lastName);
+        
+        outputHTML += `
+            <h2>${firstName} ${lastName}</h2>
+            <p>${email}</p>
+            <p>${username}</p>
+            <p>${password}</p>
+        `;
+    });
+
+    outputElement.innerHTML = outputHTML;
+}
+
+// Sample input
+const input = 'han_seojun song_kang kim_eunwoo';
+const names = input.split(' ');
+
+// Display the information on the page
+displayInfo(names);
+
+
